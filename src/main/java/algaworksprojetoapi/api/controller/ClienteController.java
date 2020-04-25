a758ee079a3204139ec7b27b47e9fbdb131259ef
Rebeca Fonseca <rebeca.bm.fonseca@gmail.com>
2020-04-25 07:00:57 -0300

    Add métodos básicos e iniciando validações

--- src/main/java/algaworksprojetoapi/api/controller/ClienteController.java
+++ src/main/java/algaworksprojetoapi/api/controller/ClienteController.java
@@ -1,34 +1,74 @@
 package algaworksprojetoapi.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import algaworksprojetoapi.domain.model.Cliente;
+import algaworksprojetoapi.domain.repository.ClienteRepository;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 	
-	@GetMapping("/clientes")
+	@Autowired
+	ClienteRepository clienteRepository;
+	
+	@GetMapping
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Rebeca Bispo");
-		cliente1.setEmail("rebecafonseca@dcc.ufrj.br");
-		cliente1.setTelefone("21 975799533");
-		
-		Cliente cliente2 = new Cliente();
-		
-		cliente2.setId(2L);
-		cliente2.setNome("lala");
-		cliente2.setEmail("joaopaulobispo@gmail.com");
-		cliente2.setTelefone("21 999999999");
+		 return clienteRepository.findAll();
 		
 		
-		return Arrays.asList(cliente1,cliente2);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Cliente> buscar(@PathVariable Long id){
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		if(cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente salvar (@Valid @RequestBody Cliente cliente) {
+		return clienteRepository.save(cliente);
+	}
+	
+	@PutMapping("{id}")
+	public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long id, 
+			@RequestBody Cliente cliente){
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		cliente.setId(id);
+		cliente = clienteRepository.save(cliente);
+		return ResponseEntity.ok().body(cliente);
 		
+	}
+	
+	@DeleteMapping("{id}")
+	public ResponseEntity<Void> deletar(@PathVariable Long id){
+		if(!clienteRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();
+		}
+		clienteRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
 	}
 
 }
